3a3fdc5410ce92480e02f0226ba0ebd9f863aa06
dbvincent0703 <dbvincent07@gmail.com>
2018-07-09 13:18:45 -0500

    create Customer class

---
+++ bankapp/src/bankapp/Customer.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankapp;
+
+/**
+ *
+ * @author vb
+ */
+public class Customer {
+    private String id, firstName,lastName, DOB;
+    public Customer(String id, String firstName, String lastName, String DOB) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.DOB = DOB;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+    
+  
+    
+}
